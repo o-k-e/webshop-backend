520875101662e2fcb53d2aeb8914d497af992ed2
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-05-23 18:04:48 +0200

    feat: add findById method to ProductS

--- src/main/java/com/ganesha/webshop/service/ProductService.java
+++ src/main/java/com/ganesha/webshop/service/ProductService.java
@@ -2,10 +2,13 @@ package com.ganesha.webshop.service;
 
 import com.ganesha.webshop.model.dto.response.ProductResponse;
 import com.ganesha.webshop.model.entity.products.Product;
+import com.ganesha.webshop.model.exception.ProductNotFoundException;
 import com.ganesha.webshop.repository.ProductRepository;
 import com.ganesha.webshop.service.mapper.ProductResponseMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -21,13 +24,15 @@ public class ProductService {
         this.productResponseMapper = new ProductResponseMapper();
     }
 
+    @GetMapping("/find-all")
     public List<ProductResponse> findAll() {
         List<Product> products = productRepository.findAll();
         return productResponseMapper.mapToProductResponseList(products);
     }
 
-    public ProductResponse findById(Long id) {
-        Product product = productRepository.findById(id).orElse(null);
+    @GetMapping("/find-by-id/{id}")
+    public ProductResponse findById(@PathVariable long id) {
+        Product product = productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException(id));
         return productResponseMapper.mapToProductResponse(product);
     }
 }
