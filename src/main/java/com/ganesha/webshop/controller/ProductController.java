9e980d9ed3300dcebb87bd9a279a2a394cf789fe
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-05-23 17:52:42 +0200

    feat: create ProductController with findAll() method

---
+++ src/main/java/com/ganesha/webshop/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.ganesha.webshop.controller;
+
+import com.ganesha.webshop.model.dto.response.ProductResponse;
+import com.ganesha.webshop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<ProductResponse> getProducts() {
+        return productService.findAll();
+    }
+}
