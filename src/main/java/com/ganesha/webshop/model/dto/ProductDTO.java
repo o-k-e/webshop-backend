5d21669987ecded5f169885710c934571b3064ca
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-05-23 16:51:48 +0200

    fix: rename ProductDTO and CategoryDTO to ProductResponse and CategoryResponse

--- src/main/java/com/ganesha/webshop/model/dto/ProductDTO.java
+++
@@ -1,9 +0,0 @@
-package com.ganesha.webshop.model.dto;
-
-import java.util.List;
-
-public record ProductDTO(String productName,
-                         String productDescription,
-                         double price,
-                         List<String> categoryNames
-) {}
---
+++ src/main/java/com/ganesha/webshop/model/dto/response/ProductResponse.java
@@ -0,0 +1,10 @@
+package com.ganesha.webshop.model.dto.response;
+
+import java.util.List;
+
+public record ProductResponse(Long id,
+                              String productName,
+                              String productDescription,
+                              double price,
+                              List<CategoryResponse> categoryNames
+) {}
