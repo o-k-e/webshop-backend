7abda42c2dbf2559a9a9bf8c6a48019ab8940042
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-05-23 16:01:24 +0200

    feat: add ProductRepository interface

---
+++ src/main/java/com/ganesha/webshop/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.ganesha.webshop.repository;
+
+import com.ganesha.webshop.model.entity.products.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
