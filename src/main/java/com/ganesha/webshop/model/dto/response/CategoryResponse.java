7cd9fd2728c7c41a01a7da453d440a36a2dba904
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-05-25 16:25:02 +0200

    feat: create separate CategoryResponse records with different parameters

--- src/main/java/com/ganesha/webshop/model/dto/response/CategoryResponse.java
+++
@@ -1,8 +0,0 @@
-package com.ganesha.webshop.model.dto.response;
-
-import java.util.List;
-
-public record CategoryResponse(Long id,
-                               String categoryName,
-                               List<ProductResponse> products) {
-}
