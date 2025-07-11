61210a9fd1e61e31838e449e03c3a9fecd0c1dd6
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-07-11 16:45:07 +0200

    feat: add image upload logic

--- src/main/java/com/ganesha/webshop/service/ImageService.java
+++ src/main/java/com/ganesha/webshop/service/ImageService.java
@@ -5,6 +5,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
 import java.util.Optional;
 
 @Service
@@ -13,7 +18,7 @@ public class ImageService {
   @Value("${fileuploader.directory}")
   private String uploadDir;
 
-    public Optional<File> serveFile(String fileName) {
+    public Optional<File> serveFile(String fileName) { //kiolvassa ha az image file benne van a folderben
         File file = new File(uploadDir, fileName);
 
         if (!file.exists()) {
