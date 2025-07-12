f24abbcb7354c8290f2a5eddd872a7a537d7d994
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-07-12 20:36:57 +0200

    chore: remove unused files

--- src/main/java/com/ganesha/webshop/service/ImageService.java
+++ src/main/java/com/ganesha/webshop/service/ImageService.java
@@ -2,14 +2,7 @@ package com.ganesha.webshop.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Base64;
 import java.util.Optional;
 
 @Service
