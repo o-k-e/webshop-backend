0887fb045b01ef216a4168011226211a195dee7b
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-07-19 23:37:06 +0200

    feat: implement JwtUtils class with token generation and validation logic build: add JJWT library dependencies (api, impl, jackson) to support JWT authentication

---
+++ src/main/java/com/ganesha/webshop/security/jwt/JwtUtils.java
@@ -0,0 +1,66 @@
+package com.ganesha.webshop.security.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
+    @Value("${WEBSHOP_JWT_SECRET}")
+    private String jwtSecret;
+
+    @Value("${JWT_EXPIRATION_MS}")
+    private int jwtExpirationMs;
+
+    public String generateJwtToken(Authentication authentication) {
+        UserDetails userPrincipal = (UserDetails) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .subject(userPrincipal.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String getUsernameFromJwtToken(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean validateJwtToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(getSigningKey())
+                    .build()
+                    .parseSignedClaims(token);    // Csak parse, hibát dob, ha érvénytelen
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            logger.error("JWT validation failed: {}", e.getMessage());
+        }
+        return false;
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
